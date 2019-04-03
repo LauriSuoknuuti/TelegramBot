59cc1ff5faa4cf6cb9a721adcf8805852b33ef59
Paul Närhi <paulnarhi@Paul-MacBook-Pro.local>
2019-04-04 00:33:37 +0300

    sää API

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telegram as tg
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import scraper
+import weather_API
+import random
 
 # Lisää tähän dictionaryyn uudet komennot ja selitteet (komento -> selite)
 commands_dict = {"/paul": "Printtaa faktan Paulista.",
                  "/menu": "Printtaa päivän ruokalistan.",
-                 "/commands": "Printtaa listan komennoista."}
+                 "/commands": "Printtaa listan komennoista.",
+                 "/sää": "Printtaa tämänhetkisen lämpötilan"}
 
 
 def paul(bot, update):
@@ -18,11 +21,27 @@ def start(bot, update):
                               "Kirjoita /commands saadaksesi lista komennoista. ")
 
 # TODO parantele outputtia, esim tyhjät pois
+# TODO laita owm palauttamaan numerot
+
+
 def menu(bot, update):
     chat_id = update.message.chat_id
     message = scraper.main()
     message_string = "\n".join(message)
     bot.send_message(chat_id=chat_id, text=message_string)
+
+
+def weather(bot, update):
+    chat_id = update.message.chat_id
+    message = weather_API.main()
+    message_string = "\n".join(message)
+    bot.send_message(chat_id=chat_id, text=message_string)
+
+
+def echo(bot, update):
+    chat_id = update.message.chat_id
+    random_text = ["vitun homo", "vitun köyhä", "vitun läski", "tapa ittes"]
+    bot.send_message(chat_id=chat_id, text=random.choice(random_text))
 
 
 def commands(bot, update):
@@ -41,6 +60,8 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("commands", commands))
     dp.add_handler(CommandHandler("menu", menu))
+    dp.add_handler(CommandHandler("sää", weather))
+    dp.add_handler(MessageHandler(Filters.reply, echo))
     updater.start_polling()
     updater.idle()
 
---
+++ weather_API.py
@@ -0,0 +1,9 @@
+import pyowm
+
+
+def main():
+    owm = pyowm.OWM("183f929d03a36cde0b893c0b59fd1d9d")
+    forecast = owm.weather_at_place("Otaniemi, finland")
+    w = forecast.get_weather()
+    current_weather = w.get_temperature(unit='celsius')
+    return current_weather
